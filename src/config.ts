11caef2735210865e1dabd00153997c8d63ee3e5
albertescote <albertescote03@gmail.com>
2025-05-10 12:59:28 +0200

    feat: add process env variable check

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,21 @@
 import * as dotenv from "dotenv";
 
 dotenv.config();
-const FRONTEND_URL = process.env.FRONTEND_URL;
-const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+const checkStrVar = (variable: string | undefined, name: string): string => {
+  if (!variable) throw new Error(`undefined variable: ${name}`);
+  return variable;
+};
+
+const FRONTEND_URL = checkStrVar(process.env.FRONTEND_URL, "FRONTEND_URL");
+const RESEND_API_KEY = checkStrVar(
+  process.env.RESEND_API_KEY,
+  "RESEND_API_KEY",
+);
 
 const REDIS = {
-  URL: process.env.REDIS_URL,
-  PORT: process.env.REDIS_PORT,
+  URL: checkStrVar(process.env.REDIS_URL, "REDIS_URL"),
+  PORT: checkStrVar(process.env.REDIS_PORT, "REDIS_PORT"),
 };
 
 export { FRONTEND_URL, RESEND_API_KEY, REDIS };
--- src/context/auth/config.ts
+++ src/context/auth/config.ts
@@ -2,19 +2,44 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const AUTHORIZE_SERVICE_PRIVATE_KEY = process.env.AUTHORIZE_SERVICE_PRIVATE_KEY;
+const checkStrVar = (variable: string | undefined, name: string): string => {
+  if (!variable) throw new Error(`undefined variable: ${name}`);
+  return variable;
+};
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const checkNumVar = (variable: string | undefined, name: string): number => {
+  if (!variable) throw new Error(`undefined variable: ${name}`);
+  try {
+    return parseInt(variable);
+  } catch (e) {
+    throw new Error(`variable ${name} is not a number: ${e}`);
+  }
+};
+
+const AUTHORIZE_SERVICE_PRIVATE_KEY = checkStrVar(
+  process.env.AUTHORIZE_SERVICE_PRIVATE_KEY,
+  "AUTHORIZE_SERVICE_PRIVATE_KEY",
+);
+
+const GOOGLE_CLIENT_ID = checkStrVar(
+  process.env.GOOGLE_CLIENT_ID,
+  "GOOGLE_CLIENT_ID",
+);
+const GOOGLE_CLIENT_SECRET = checkStrVar(
+  process.env.GOOGLE_CLIENT_SECRET,
+  "GOOGLE_CLIENT_SECRET",
+);
 
 const TOKEN_TYPE = "Bearer";
 
-const ACCESS_TOKEN_EXPIRES_IN_SECONDS = Number(
+const ACCESS_TOKEN_EXPIRES_IN_SECONDS = checkNumVar(
   process.env.ACCESS_TOKEN_EXPIRES_IN_SECONDS,
+  "ACCESS_TOKEN_EXPIRES_IN_SECONDS",
 );
 
-const REFRESH_TOKEN_EXPIRES_IN_SECONDS = Number(
+const REFRESH_TOKEN_EXPIRES_IN_SECONDS = checkNumVar(
   process.env.REFRESH_TOKEN_EXPIRES_IN_SECONDS,
+  "REFRESH_TOKEN_EXPIRES_IN_SECONDS",
 );
 
 const TOKEN_ISSUER = "learning-platform-backend";
