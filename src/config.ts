1f5028ad406941bc8f601131115863c8d8773b80
albertescote <albertescote03@gmail.com>
2025-05-12 13:54:47 +0200

    fix: merge conflicts

--- src/config.ts
+++ src/config.ts
@@ -7,15 +7,22 @@ const checkStrVar = (variable: string | undefined, name: string): string => {
   return variable;
 };
 
-const FRONTEND_URL = checkStrVar(process.env.FRONTEND_URL, "FRONTEND_URL");
+const FRONTEND_PAGE_URL = checkStrVar(
+  process.env.FRONTEND_PAGE_URL,
+  "FRONTEND_PAGE_URL",
+);
+const FRONTEND_AUTH_URL = checkStrVar(
+  process.env.FRONTEND_AUTH_URL,
+  "FRONTEND_AUTH_URL",
+);
+const FRONTEND_APP_URL = checkStrVar(
+  process.env.FRONTEND_APP_URL,
+  "FRONTEND_APP_URL",
+);
 const RESEND_API_KEY = checkStrVar(
   process.env.RESEND_API_KEY,
   "RESEND_API_KEY",
 );
-const FRONTEND_PAGE_URL = process.env.FRONTEND_PAGE_URL;
-const FRONTEND_AUTH_URL = process.env.FRONTEND_AUTH_URL;
-const FRONTEND_APP_URL = process.env.FRONTEND_APP_URL;
-const RESEND_API_KEY = process.env.RESEND_API_KEY;
 
 const REDIS = {
   URL: checkStrVar(process.env.REDIS_URL, "REDIS_URL"),
