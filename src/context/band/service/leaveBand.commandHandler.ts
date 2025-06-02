859543d13a541ff55d24d004a1c2e14666daf5e8
albertescote <albertescote03@gmail.com>
2025-06-02 11:21:25 +0200

    feat: refactor LeaveBandCommandHandler to use CommandHandler decorator and improve code readability

--- src/context/band/service/leaveBand.commandHandler.ts
+++ src/context/band/service/leaveBand.commandHandler.ts
@@ -1,9 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { ICommandHandler } from "@nestjs/cqrs";
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { BandRepository } from "../infrastructure/band.repository";
 import { LeaveBandCommand } from "./leaveBand.command";
 import { BandNotFoundException } from "../exceptions/bandNotFoundException";
-import Band from "../domain/band";
 import BandId from "../../shared/domain/bandId";
 import UserId from "../../shared/domain/userId";
 import { UserIsNotMemberOfBandException } from "../exceptions/userIsNotMemberOfBandException";
@@ -11,11 +10,16 @@ import { AdminCannotLeaveBandException } from "../exceptions/adminCannotLeaveBan
 import { BandRole } from "../domain/bandRole";
 
 @Injectable()
-export class LeaveBandCommandHandler implements ICommandHandler<LeaveBandCommand> {
+@CommandHandler(LeaveBandCommand)
+export class LeaveBandCommandHandler
+  implements ICommandHandler<LeaveBandCommand>
+{
   constructor(private readonly bandRepository: BandRepository) {}
 
   async execute(command: LeaveBandCommand): Promise<void> {
-    const band = await this.bandRepository.getBandById(new BandId(command.bandId));
+    const band = await this.bandRepository.getBandById(
+      new BandId(command.bandId),
+    );
 
     if (!band) {
       throw new BandNotFoundException(command.bandId);
@@ -23,16 +27,15 @@ export class LeaveBandCommandHandler implements ICommandHandler<LeaveBandCommand
 
     const members = band.toPrimitives().members;
 
-    const userIsMember = members.some(
-      (member) => member.id === command.userId,
-    );
+    const userIsMember = members.some((member) => member.id === command.userId);
 
     if (!userIsMember) {
       throw new UserIsNotMemberOfBandException(command.userId, command.bandId);
     }
 
     const userIsAdmin = members.some(
-      (member) => member.id === command.userId && member.role === BandRole.ADMIN,
+      (member) =>
+        member.id === command.userId && member.role === BandRole.ADMIN,
     );
 
     if (userIsAdmin) {
